e25f765bebf774559f2f246f79a739b6feec6158
AiRikux <nabilahqanuwar@gmail.com>
2022-12-05 00:07:39 +1100

    Update introduction.js

--- C Learn JavaScript/introduction.js
+++ C Learn JavaScript/introduction.js
@@ -1,4 +1,17 @@
+// Printing to the console
 console.log('JavaScript');
 console.log(2011);
 console.log('Woohoo! I love to code! #codecademy');
-console.log('20.49');
+console.log(20.49);
+
+// Arithmetic
+console.log(23 + 3.5);
+console.log(2022 - 1969);
+console.log(65 / 240);;
+console.log(0.2708*100)
+
+// String Concatenation
+console.log('Hello' + 'World');
+console.log('Hello' + ' ' + 'World');
+
+// Properties
