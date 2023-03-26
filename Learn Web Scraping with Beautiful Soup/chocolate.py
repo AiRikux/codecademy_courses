d07eb75ac8bddb42761ce159f6b9cd81de830ef3
AiRikux <nabilahqanuwar@gmail.com>
2023-03-27 02:57:43 +1100

    Update chocolate.py
    Created table with two values from scraping

--- Learn Web Scraping with Beautiful Soup/chocolate.py
+++ Learn Web Scraping with Beautiful Soup/chocolate.py
@@ -1,9 +1,10 @@
-import seaborn as sns# codecademylib3_seaborn
+import seaborn as sns # codecademylib3_seaborn
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+
 
 # 1 Explore the webpage displayed in the browser. What elements could be useful to scrape here? Which elements do we not want to scrape?
 
@@ -24,5 +25,20 @@ ratings = []
 for rate in soup.select(".Rating")[1:]:
 	ratings.append(float(rate.get_text()))
 
-plt.hist(ratings).show()
+plt.hist(ratings)
+# plt.show()
 
+# 5 Which chocolatier makes the best chocolate?
+# get Company values
+companies = []
+# skip first value as it is a heading
+for comp in soup.select(".Company")[1:]:
+	companies.append(comp.get_text())
+# create dataframe with companies and ratings only
+chocolate_df = pd.DataFrame(list(zip(companies, ratings)), columns=["Companies", "Ratings"])
+# groupby company to see average
+print(chocolate_df.groupby("Companies").mean().nlargest(10, 'Ratings'))
+
+# 6 Is more cacao better?
+# get CocoaPercent values
+cocoaper = []
