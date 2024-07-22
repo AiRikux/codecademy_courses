ee5c8af60c73597a694d33f4facb719fe0123601
AiRikux <nabilahqanuwar@gmail.com>
2024-07-22 22:48:56 +1000

    Update blossom.py
    codecademy solution bug fixes, not major just need to delete some things

--- CP Computer Science/CS102/5 - Hash Maps/blossom.py
+++ CP Computer Science/CS102/5 - Hash Maps/blossom.py
@@ -1,4 +1,5 @@
 from linked_list import Node, LinkedList
+from blossom_lib import flower_definitions
 
 
 class HashMap:
@@ -22,40 +23,44 @@ class HashMap:
 		payload = Node([key, value])
 		list_at_array = self.array[array_index]
 
-		# make a marker to see if we found the same key
-		found = False
 		for item in list_at_array:
 			if item[0] == key:
 				# if we find a key item that is the same as the key were trying to assign
 				# we replace
 				item[1] = value
-				found = True
-		if found:
-			list_at_array.insert(payload)
+				return
+		list_at_array.insert(payload)
 
 	def retrieve(self, key):
 		hash_code = self.hash(key)
 		array_index = self.compress(hash_code)
 
-		list_at_index = [LinkedList() for i in range(self.array_index)]
+		list_at_index = self.array[array_index]
 
 		# make a marker to see if we found the same key
 		found = False
 		for item in list_at_index:
 			if item[0] == key:
 				# if we find a key item that is the same as the key were trying to assign
-				found = True
-		if found:
-			return item[1]
-		else:
-			return None
+				return item[1]
+			else:
+				return None
 
-		payload = self.array[array_index]
+	# payload = self.array[array_index]
 
-		if payload is not None:
-			if payload[0] == key:
-				return payload[1]
+	# if payload is not None:
+	#  if payload[0] == key:
+	#    return payload[1]
 
-		else:
-			return None
+	# else:
+	#  return None
 
+
+# see it in action
+# create hashmap with the same length as how many types of flowers there are
+blossom = HashMap(len(flower_definitions))
+# assign for each flower
+for flower in flower_definitions:
+	blossom.assign(flower[0], flower[1])
+# retrieve something
+print(blossom.retrieve('daisy'))
